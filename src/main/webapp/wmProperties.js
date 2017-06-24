698a2dfe490ec88e7a8c269b8fa23d694df0c31d
root <root@ca445d4f2acb>
2017-06-24 18:07:01 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_QtqiyeWQrO",
+  "homePage" : "Main",
+  "name" : "Auto_QtqiyeWQrO",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
